bd8cc887d8bc6ab099297a75c61e817fc212b744
Agunivers <93704356+Agunivers@users.noreply.github.com>
2022-07-26 15:51:14 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -24,7 +24,7 @@ im a group manager bot maintained by  [this person](tg://user?id={}).
 
 My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
+This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/Agunivers/marie),
 you can create clone same like me..
 
 For more commands click /help...
